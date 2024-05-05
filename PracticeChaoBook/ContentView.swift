214e10dfd3564308d239dbf406c1a83457d28cfc
KC <kc995266@gmail.com>
2024-05-05 23:03:54 +0800

    [新增] initial project

---
+++ PracticeChaoBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeChaoBook
+//
+//  Created by Bruce Chen on 2024/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeChaoBook/PracticeChaoBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeChaoBookApp.swift
+//  PracticeChaoBook
+//
+//  Created by Bruce Chen on 2024/5/5.
+//
+
+import SwiftUI
+
+@main
+struct PracticeChaoBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
